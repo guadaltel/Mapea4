cfcee347565dd84d67471c09cfb4742de3254b0b
Carmen <carmenmarquez@guadaltel.com>
2025-02-06 23:01:43 +0100

    se restaura test

--- mapea-js/test/development/cluster-heavy-wfs.js
+++ mapea-js/test/development/cluster-heavy-wfs.js
@@ -1,165 +1,134 @@
 import { map } from 'M/mapea';
 import WFS from 'M/layer/WFS';
 import StylePolygon from 'M/style/Polygon';
-import Generic from 'M/style/Generic';
 import StylePoint from 'M/style/Point';
-import Cluster from 'M/style/Cluster';
-import Category from 'M/style/Category';
-import { SELECT_FEATURES as SelectFeaturesEvt } from 'M/event/eventtype';
-
+import StyleCluster from 'M/style/Cluster';
+import StyleCategory from 'M/style/Category';
 
 const mapjs = map({
   container: 'map',
   controls: ['layerswitcher'],
 });
 
-const diagrama_circular = new WFS({
-  url: 'https://www.juntadeandalucia.es/medioambiente/mapwms/REDIAM_Mapa_inversiones_Andalucia?',
-  legend: 'Grafico inversiones por provincia',
-  name: 'diagrama_circular_importe',
-  geometry: 'POINT'
+const campamentos = new WFS({
+  name: 'campamentos',
+  url: 'http://geostematicos-sigc.juntadeandalucia.es/geoserver/wfs',
+  namespace: 'sepim',
+  geometry: 'POINT',
+  extract: true,
+});
+mapjs.addLayers(campamentos);
+
+const reservaST = new StylePolygon({ fill: { color: 'rgb(223, 115, 255)' } });
+const reservas = new WFS({
+  name: 'reservas_biosfera',
+  namespace: 'reservas_biosfera',
+  legend: 'Reservas biosferas',
+  geometry: 'POLYGON',
+  url: 'https://www.juntadeandalucia.es/medioambiente/mapwms/REDIAM_WFS_Patrimonio_Natural?',
+  version: '1.1.0',
 }, {
   getFeatureOutputFormat: 'geojson',
-  describeFeatureTypeOutputFormat: 'geojson'
+  describeFeatureTypeOutputFormat: 'geojson',
+});
+reservas.setStyle(reservaST);
+mapjs.addLayers(reservas);
+
+mapjs.getFeatureHandler().removeLayer(reservas);
+
+// Estilos para categorización
+const categoryStyle = new StyleCategory('categoria', {
+  Primera: new StylePoint({
+    icon: {
+      // src: 'https://image.flaticon.com/icons/svg/34/34697.svg',
+      src: 'https://raw.githubusercontent.com/VectorLogoZone/vectorlogozone/b16a80dc8527670a8bc79fe4f80ff33435b8606c/www/logos/google/google-icon.svg',
+      scale: 0.1,
+    },
+  }),
+  Segunda: new StylePoint({
+    icon: {
+      // src: 'https://image.flaticon.com/icons/svg/34/34651.svg',
+      src: 'https://raw.githubusercontent.com/VectorLogoZone/vectorlogozone/b16a80dc8527670a8bc79fe4f80ff33435b8606c/www/logos/adobe/adobe-icon.svg',
+      scale: 0.1,
+    },
+  }),
+  Tercera: new StylePoint({
+    icon: {
+      // src: 'https://image.flaticon.com/icons/svg/34/34654.svg',
+      src: 'https://raw.githubusercontent.com/VectorLogoZone/vectorlogozone/b16a80dc8527670a8bc79fe4f80ff33435b8606c/www/logos/apple/apple-icon.svg',
+      scale: 0.1,
+    },
+  }),
 });
 
-mapjs.addWFS(diagrama_circular);
-
-const cluster_inversion = new WFS({
-	url: 'https://www.juntadeandalucia.es/medioambiente/mapwms/REDIAM_Mapa_inversiones_Andalucia?',
-	legend: 'Mapa Cluster Inversiones Andalucía',
-	name: 'inversiones_puntual',
-	geometry: 'MPOINT',
-	}, {
-    visibility: false,
-    getFeatureOutputFormat: 'geojson',
-	describeFeatureTypeOutputFormat: 'geojson'
-});	
-
-cluster_inversion.on(SelectFeaturesEvt, function (features, evt) {
-	if (features[0] instanceof M.ClusteredFeature) {
-    console.log("Es un cluster:", features[0].getAttribute("features"));
-	} else {
-			console.log("NO es un cluster:", features);
-		}
-	});
-
-mapjs.addLayers(cluster_inversion);
-
-   //Estilos para categorización
-   let Inv_uso_publico = new Generic({
-    point: {
-    icon: {
-      src: 'http://localhost:8080/test/development/headshot.png',
-      scale: 1
-    },
-  }});
-
-  let Inv_medio_natural = new Generic({
-    point: {
-    icon: {
-      src: 'http://localhost:8080/test/development/headshot.png',
-      scale: 1
-    },
-  }});
-
-  let Inv_educac_sostenibilidad = new Generic({
-    point: {
-    icon: {
-      src: 'http://localhost:8080/test/development/headshot.png',
-      scale: 1
-    },
-  }});
-
-  let Inv_econ_circular = new Generic({
-    point: {
-    icon: {
-      src: 'http://localhost:8080/test/development/headshot.png',
-      scale: 1
-    },
-  }});
-  
-  let categoryStyle = new Category('area_temat', {
-  'USO PÚBLICO': Inv_uso_publico,
-    'MEDIO NATURAL': Inv_medio_natural,
-    'EDUCACIÓN AMBIENTAL': Inv_educac_sostenibilidad, 
-    'ECONOMÍA CIRCULAR': Inv_econ_circular
-  });
-
-  //Estilo para cluster
-  let clusterOptions = {
-    ranges: [{
-      min: 2,
-      max: 5,
-  style: new Generic({
-        point: {
-        stroke: {
-          color: '#0a443b'
-        },
-        fill: {
-          color: '#91f162',
-        },
-        radius: 20
-      }})
-    }, {
-      min: 6,
-      max: 9,		
-  style: new Generic({
-        point: {
-        stroke: {
-          color: '#0a443b'
-        },
-        fill: {
-          color: '#0adb3b',
-        },  
-        radius: 25
-      }})
-    }, {
-      min: 10,
-      max: 14,
-     style: new Generic({
-        point: {
-        stroke: {
-          color: '#0a443b'
-        },
-        fill: {
-          color: '#0aae3b',
-        },
-        radius: 30
-      }})
-    },{
-      min: 15,
-      max: 20,
-      style: new Generic({
-        point: {
-        stroke: {
-          color: '#0a443b'
-        },
-        fill: {
-          color: '#0A793B',
-        },
-        radius: 40
-      }})
-    }],
-    animated: true,
-    hoverInteraction: true,
-    displayAmount: true,
-    distance: 30,
-    maxFeaturesToSelect: 8
-  };
-  let vendorParameters = {
-    distanceSelectFeatures: 35,
-    convexHullStyle: {
-      fill: {
-        color: '#000000',
-        opacity: 0.5,
-      },
+// Estilo para cluster
+const clusterOptions = {
+  ranges: [{
+    min: 2,
+    max: 4,
+    style: new StylePoint({
       stroke: {
-        color: '#000000',
-        width: 1,
+        color: '#5789aa',
       },
+      fill: {
+        color: '#99ccff',
+      },
+      radius: 20,
+    }),
+  }, {
+    min: 5,
+    max: 9,
+    style: new StylePoint({
+      stroke: {
+        color: '#5789aa',
+      },
+      fill: {
+        color: '#3399ff',
+      },
+      radius: 30,
+    }),
+  }, {
+    min: 10,
+    max: 15,
+    style: new StylePoint({
+      stroke: {
+        color: '#5789aa',
+      },
+      fill: {
+        color: '#004c99',
+      },
+      radius: 40,
+    }),
+  }],
+  animated: true,
+  hoverInteraction: true,
+  displayAmount: true,
+  distance: 80,
+  maxFeaturesToSelect: 7,
+};
+const vendorParameters = {
+  distanceSelectFeatures: 25,
+  convexHullStyle: {
+    fill: {
+      color: '#000000',
+      opacity: 0.5,
     },
-  };
-let clusterStyle = new Cluster(clusterOptions, vendorParameters);
-cluster_inversion.setStyle(categoryStyle);
-cluster_inversion.setStyle(clusterStyle);
+    stroke: {
+      color: '#000000',
+      width: 1,
+    },
+  },
+};
+const clusterStyle = new StyleCluster(clusterOptions, vendorParameters);
+
+window.setStyleCat = () => {
+  campamentos.setStyle(categoryStyle);
+};
+
+window.setStyleCluster = () => {
+  campamentos.setStyle(clusterStyle);
+};
+
+campamentos.setStyle(clusterStyle);
+
+window.mapjs = mapjs;
